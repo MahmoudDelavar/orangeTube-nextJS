2d8ad32bf865452d8ebe6e4fab94570d04b548a1
mahmoud <mahmoud.d.p7@gmail.com>
2022-12-19 23:47:03 +0330

    Create Layout Component

---
+++ components/util/views/layout.js
@@ -0,0 +1,24 @@
+import Navbar from "./nabar";
+import Banner from "./banner";
+import { Col, Container, Row } from "react-bootstrap";
+
+//===========================================================
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Banner />
+      <Container fluid>
+        <Row>
+          <Col xxl={12} xl={12} lg={12} md={12}>
+            <Navbar />
+          </Col>
+          <Col xxl={9} xl={9} lg={10} md={11}>
+            <main>{children}</main>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Layout;
